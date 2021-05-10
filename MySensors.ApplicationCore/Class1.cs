3f1bf0c7571bd49af7adfba5f5812014da421dc9
invisy <invisy@users.noreply.github.com>
2021-05-10 23:53:16 +0300

    Init projects

---
+++ MySensors.ApplicationCore/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MySensors.ApplicationCore
+{
+    public class Class1
+    {
+    }
+}
---
+++ MySensors.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MySensors.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
